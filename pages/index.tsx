f036eebacd6f210f28e97a53286f26ee68247d70
Febriyoga BS <47034901+masblangkon@users.noreply.github.com>
2021-12-22 19:54:15 +0700

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { Layout } from "@/components/layout"
+import Hero from "@/blocks/hero"
+
+export default function IndexPage() {
+  return (
+    <Layout>
+      <Hero
+        heading="Welcome to SAMA BANDUNG"
+        text="Portofolio Web App"
+        image="/images/placeholder.jpg"
+      />
+    </Layout>
+  )
+}
---
+++ src/config.ts
@@ -0,0 +1,24 @@
+export default {
+  site: {
+    branding: {
+      name: "SAMA.",
+    },
+    copyright: `© ${new Date().getFullYear()} | SAMA Bandung.`,
+    links: [
+      {
+        title: "Home",
+        href: "/",
+      },
+      {
+        title: "First Portofolio",
+        href: "https://dalang.vercel.app/",
+        target: "_blank"
+      },
+      {
+        title: "GitHub",
+        href: "https://github.com/orgs/SAMA-BANDUNG/repositories",
+        target: "_blank"
+      },
+    ],
+  },
+}
